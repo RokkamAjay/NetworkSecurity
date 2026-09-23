78faea7754c48721d448d0fc90259fcb55bae745
RokkamAjay <ajayrokkam38@gmail.com>
2026-09-23 21:56:04 +0530

    Trainer Evaluation and mlflow

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -16,6 +16,9 @@ TEST_FILE_NAME : str =  'test.csv'
 
 SCHEMA_FILE_PATH = os.path.join('data_schema','schema.yaml')
 
+SAVED_MODEL_DIR = os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
 """
 --------------> Data Ingestion releated Constant Starts with  DATA_INGESTION VAR NAME
 """
@@ -52,3 +55,17 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     'n_neighbors': 3,
     'weights':'uniform',
 }
+
+DATA_TRANSFORMATION_TRAIN_FILE_PATH : str = 'train.npy'
+DATA_TRANSFORMATION_TEST_FILE_PATH : str = 'test.npy'
+
+"""
+Model Trainer Releated Constant start with mode  Trainer Var Name 
+
+"""
+
+MODEL_TRAINER_DIR_NAME: str = 'model_trainer'
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = 'trained_model'
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = 'model.pkl'
+MODEL_TRAINER_EXCEPTED_SCORE: str = 0.6
+MODEL_TRAINED_OVER_FITTING_UNDER_FITTING_THRESHOLD: float = 0.05
